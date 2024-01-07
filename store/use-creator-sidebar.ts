a2e6014bc2ea72a07c593ba7c2a49312adc6e31f
Your Name <chetandasa2814@gmail.com>
2024-01-07 15:54:37 +0530

    ..

--- store/use-creator-sidebar.ts
+++ store/use-creator-sidebar.ts
@@ -11,5 +11,3 @@ export const useCreatorSidebar = create<CreatorSidebarStore>((set) => ({
   onExpand: () => set(() => ({ collapsed: false })),
   onCollapse: () => set(() => ({ collapsed: true })),
 }));
-
-
